0cc56e3a2d55efc64c1580d9aec088f1370708b9
DaemonLibra <40869874+daemonLibra@users.noreply.github.com>
2020-12-16 10:12:07 +0100

    code clean up and refactoring

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -19,76 +19,61 @@ public class NeuralNetwork {
     }
 
     //calculates the matrix product, in this case we have a 4x3 matrix * a 3x1 matrix/vector
-    private double[] dotProduct(double[][] a, double[] b){
-        double[][] products = new double[4][3];
-        double[] result = new double[4];
+    private double dotProduct(double[] a, double[] b){
+        double[] products = new double[a.length];
 
         //first we do the multiplication
         for(int i = 0; i < a.length; i++){
-            for(int j = 0; j < a[i].length; j++){
-                products[i][j] = a[i][j] * b[j];
-            }
+            products[i] = a[i] * b[i];
         }
 
         //then we calculate the product
+        double result = 0;
         for(int x = 0; x <products.length; x++){
-            double sum = 0;
-            for (int y = 0; y <products[x].length; y++){
-                sum += products[x][y];
+                result += products[x];
             }
-            result[x] = sum;
-        }
         return result;
     }
 
-    //like the other matrix product but this time the 4x3 matrix is an 3x4 matrix
-    private double[] dotOtherProduct(double[][] a, double[] b){
-        double[][] products = new double[4][3];
-        double[] result = new double[3];
+    private double[][] turnMatrix(double[][] a){
+        double[][] result = new double[a[0].length][a.length];
 
         for(int i = 0; i < a[i].length; i++){
             for(int j = 0; j < a.length; j++){
-                products[j][i] = a[j][i] * b[j];
+                result[i][j] = a[j][i];
             }
-        }
-
-        for(int x = 0; x <products[x].length; x++){
-            double sum = 0;
-            for (int y = 0; y <products.length; y++){
-                sum += products[y][x];
-            }
-            result[x] = sum;
         }
         return result;
     }
 
     //this function calculates the error rate from the given output and the estimated output
-    private double[] calcError(double[] outputReal, double[] outputsEst){
-        double[] result = new double[4];
-        for (int i = 0; i < outputReal.length; i++){
-            result[i] = outputReal[i]-outputsEst[i];
-        }
-        return result;
+    private double calcError(double outputReal, double outputsEst){
+        return outputReal-outputsEst;
     }
 
     //here the error rates are being multiplied by the estiamted output,
     // if the error rate was low then the est. output was close to being right
-    private double[] adjustmentInputs(double[] error, double[] output){
-        double[] result = new double[4];
-        for (int i = 0; i < error.length; i++){
-            result[i] = error[i] * sigmoidDerivative(output[i]);
-        }
-        return result;
+    private double adjustmentInputs(double error, double output){
+        return error * sigmoidDerivative(output);
     }
 
     public void train(double inputs[][], double[] outputs, int trainingIteration){
         for (int i = 0; i < trainingIteration;i++){
 
-            double[] output = think(inputs);
+            double[] output = new double[outputs.length];
+            double[] error = new double[output.length];
+            double[] loss = new double[error.length];
+            for(int cInputs = 0; cInputs < inputs.length; cInputs++){
+                output[cInputs] = think(inputs[cInputs]);
+                error[cInputs] = calcError(outputs[cInputs], output[cInputs]);
+                loss[cInputs] = adjustmentInputs(error[cInputs], output[cInputs]);
+            }
 
-            double[] error = calcError(outputs,output);
-
-            double[] adjustment = dotOtherProduct(inputs, adjustmentInputs(error, output));
+            double[][] turnedInput = turnMatrix(inputs);
+            double[] adjustment = new double[weights.length];
+            for(int k = 0; k < adjustment.length; k++){
+               adjustment[k] = dotProduct(turnedInput[k], loss);
+            }
 
             //the training weights are being adjusted
             for (int j = 0; j < weights.length; j++) {
@@ -110,31 +95,11 @@ public class NeuralNetwork {
         System.out.println("Loss: " + loss + " Iteration: " + iteration);
     }
 
-    private double[] think(double inputs[][]){
+    public double think(double inputs[]){
 
-        double[] result = dotProduct(inputs, weights);
+        double result = dotProduct(inputs, weights);
 
-        for (int a = 0; a < result.length; a++){
-            result[a] = sigmoidNormalisation(result[a]);
-        }
-
-        return result;
-    }
-
-    public double thinking(double inputs[]){
-
-        double[] result = new double[3];
-
-        for (int i = 0; i < inputs.length; i++){
-            result[i] = inputs[i] * weights[i];
-        }
-
-        double sum = 0;
-        for (int a = 0; a < result.length; a++){
-            sum += result[a];
-        }
-
-        return sigmoidNormalisation(sum);
+        return sigmoidNormalisation(result);
     }
 
     //creating random weights at the begining, 3x1 matrix
@@ -146,5 +111,4 @@ public class NeuralNetwork {
         }
         System.out.println("Weights generation done!");
     }
-
 }
